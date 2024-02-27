b5d88af077b5df4e34042bdcc50b481dce5b0898
jeny-mdr <mjeny22@tbc.edu.np>
2024-02-27 13:06:18 +0545

    Week1 Added

---
+++ Week1/DiamondShape.java
@@ -0,0 +1,12 @@
+class DiamondShape{
+ public static void main(String args[]){
+  
+  System.out.println("   *   ");
+  System.out.println("  ***  ");
+  System.out.println(" ***** ");
+  System.out.println("*******");
+  System.out.println(" ***** ");
+  System.out.println("  ***  ");
+  System.out.println("   *   ");
+ }
+}
---
+++ Week1/PersonalDetails.java
@@ -0,0 +1,16 @@
+class PersonalDetails{
+ public static void main(String args[]){
+
+ String name = "Jeny Manandhar";
+ String address = "Bhaktapur";
+ int age = 20;
+ String phoneNumber = "9841164797";
+
+  System.out.println("Personal Details");
+  System.out.println("Name:"+name);
+  System.out.println("Address:"+address);
+  System.out.println("Age:"+age);
+  System.out.println("Phone Number:"+phoneNumber);
+
+ }
+}
